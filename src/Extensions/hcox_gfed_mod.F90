6406ea2da6b0a4671064e825d6da01dea5959b34
Max Harvey <max.harvey@env.ethz.ch>
2026-07-09 15:24:11 +0200

    GFED: inject 100% BB emiss at L20 (~4 km, 47L)

--- src/Extensions/hcox_gfed_mod.F90
+++ src/Extensions/hcox_gfed_mod.F90
@@ -222,15 +222,9 @@ CONTAINS
 
     TYPE(MyInst), POINTER :: Inst
 
-!==============================================================================
-! This code is required for the vertical distribution of biomass burning emiss.
-! We will keep it here for a future implementation. (mps, 4/24/17)
-!    INTEGER             :: I, J, L, N, M
-!    INTEGER             :: PBL_MAX
-!    REAL(hp)            :: PBL_FRAC, F_OF_PBL, F_OF_FT
-!    REAL(hp)            :: DELTPRES, TOTPRESFT
-!    REAL(hp), TARGET    :: SpcArr3D(HcoState%NX,HcoState%NY,HcoState%NZ)
-!==============================================================================
+    INTEGER, PARAMETER  :: HIGH_INJECT_LEVEL = 20  ! inject 100% here (~4 km, 47L)
+    INTEGER             :: I, J
+    REAL(hp), TARGET    :: SpcArr3D(HcoState%NX,HcoState%NY,HcoState%NZ)
 
     !=================================================================
     ! HCOX_GFED_Run begins here!
@@ -335,11 +329,7 @@ CONTAINS
        ! SpcArr are the total biomass burning emissions for this
        ! species. TypArr are the emissions from a given source type.
        SpcArr   = 0.0_hp
-!==============================================================================
-! This code is required for the vertical distribution of biomass burning emiss.
-! We will keep it here for a future implementation. (mps, 4/24/17)
-!       SpcArr3D = 0.0_hp
-!==============================================================================
+       SpcArr3D = 0.0_hp
 
        ! Calculate emissions for all source types
        DO M = 1, N_EMFAC
@@ -449,82 +439,18 @@ CONTAINS
            RETURN
        ENDIF
 
-!==============================================================================
-! This code is required for the vertical distribution of biomass burning emiss.
-! We will keep it here for a future implementation. (mps, 4/24/17)
-!
-!       !--------------------------------------------------------------------
-!       ! For grid boxes with emissions, distribute 65% to PBL and 35% to FT
-!       !--------------------------------------------------------------------
-!       DO J = 1, HcoState%Ny
-!       DO I = 1, HcoState%Nx
-!
-!          IF ( SpcArr(I,J) > 0e+0_hp ) THEN
-!
-!             ! Initialize
-!             PBL_MAX  = 1
-!             F_OF_PBL = 0e+0_hp
-!             F_OF_FT  = 0e+0_hp
-!             DELTPRES = 0e+0_hp
-!
-!             ! Determine PBL height
-!             DO L = HcoState%NZ, 1, -1
-!                IF ( ExtState%FRAC_OF_PBL%Arr%Val(I,J,L) > 0.0_hp ) THEN
-!                   PBL_MAX = L
-!                   EXIT
-!                ENDIF
-!             ENDDO
-!
-!             ! Loop over the boundary layer
-!             DO L = 1, PBL_MAX
-!
-!                ! Fraction of PBL that box (I,J,L) makes up [unitless]
-!                F_OF_PBL = ExtState%FRAC_OF_PBL%Arr%Val(I,J,L)
-!
-!                ! Add only 65% biomass burning source to PBL
-!                ! Distribute emissions thru the entire boundary layer
-!                ! (mps from evf+tjb, 3/10/17)
-!                SpcArr3D(I,J,L) = SpcArr(I,J) * PBL_FRAC * F_OF_PBL
-!
-!             ENDDO
-!
-!
-!             ! Total thickness of the free troposphere [hPa]
-!             ! (considered here to be 10 levels above the PBL)
-!             TOTPRESFT = HcoState%Grid%PEDGE%Val(I,J,PBL_MAX+1) - &
-!                         HcoState%Grid%PEDGE%Val(I,J,PBL_MAX+11)
-!
-!
-!             ! Loop over the free troposphere
-!             DO L = PBL_MAX+1, PBL_MAX+10
-!
-!                ! Thickness of level L [hPa]
-!                DELTPRES = HcoState%Grid%PEDGE%Val(I,J,L) - &
-!                           HcoState%Grid%PEDGE%Val(I,J,L+1)
-!
-!                ! Fraction of FT that box (I,J,L) makes up [unitless]
-!                F_OF_FT = DELTPRES / TOTPRESFT
-!
-!                ! Add 35% of biomass burning source to free troposphere
-!                ! Distribute emissions thru 10 model levels above the BL
-!                ! (mps from evf+tjb, 3/10/17)
-!                SpcArr3D(I,J,L) = SpcArr(I,J) * (1.0-PBL_FRAC) * F_OF_FT
-!
-!             ENDDO
-!
-!          ENDIF
-!
-!       ENDDO
-!       ENDDO
-!
-!       ! Add flux to HEMCO emission array
-!       ! Now 3D flux (mps, 3/10/17)
-!       CALL HCO_EmisAdd( HcoState,   SpcArr3D, HcoIDs(N), &
-!                         RC,        ExtNr=ExtNr )
-!==============================================================================
+       !--------------------------------------------------------------------
+       ! Inject 100% of fire emissions into level HIGH_INJECT_LEVEL (~4 km)
+       !--------------------------------------------------------------------
+       DO J = 1, HcoState%Ny
+       DO I = 1, HcoState%Nx
+          SpcArr3D(I,J,HIGH_INJECT_LEVEL) = SpcArr(I,J)
+       ENDDO
+       ENDDO
 
-       ! Add flux to HEMCO emission array
-       CALL HCO_EmisAdd( HcoState, SpcArr, Inst%HcoIDs(N), RC, ExtNr=Inst%ExtNr )
+       ! Add flux to HEMCO emission array (3D)
+       CALL HCO_EmisAdd( HcoState, SpcArr3D, Inst%HcoIDs(N), &
+                         RC, ExtNr=Inst%ExtNr )
        IF ( RC /= HCO_SUCCESS ) THEN
           MSG = 'HCO_EmisAdd error: ' // TRIM(HcoState%Spc(Inst%HcoIDs(N))%SpcName)
           CALL HCO_ERROR(MSG, RC )
